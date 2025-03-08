0f62710cd3242b39ddb1c995e733d5acb5a2f2be
Domenic Marcelli <spartanxd31@vt.edu>
2025-03-08 14:54:05 -0500

    added Jonas's encoder code to the comms

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "comm.hpp"
 #include <Arduino.h>
 #include <ArduinoJson.h>
+#include <RotaryEncoder.h>
 #include <stdlib.h>
 // put function declarations here:
 // int myFunction(int, int);
@@ -25,10 +26,29 @@ const int ledPin = 13;
 #define BL_PIN_1 19
 #define BL_PIN_2 18
 
+#define EL_PIN_1 28
+#define EL_PIN_2 29
+
+#define ER_PIN_1 6
+#define ER_PIN_2 7
+
+#define EC_PIN_1 17
+#define EC_PIN_2 16
+
+#define COUNTS_PER_REV 64
+#define GEAR_RATIO 100
+
 MotorControl FR = MotorControl(FR_PIN_1, FR_PIN_1);
 MotorControl FL = MotorControl(FL_PIN_1, FL_PIN_2);
 MotorControl BR = MotorControl(BR_PIN_1, BR_PIN_2);
 MotorControl BL = MotorControl(BL_PIN_1, BL_PIN_2);
+
+RotaryEncoder Encoder_Left =
+    RotaryEncoder(EL_PIN_1, EL_PIN_2, RotaryEncoder::LatchMode::TWO03);
+RotaryEncoder Encoder_Right =
+    RotaryEncoder(ER_PIN_1, ER_PIN_2, RotaryEncoder::LatchMode::TWO03);
+RotaryEncoder Encoder_Center =
+    RotaryEncoder(EC_PIN_1, EC_PIN_2, RotaryEncoder::LatchMode::TWO03);
 
 devices activeDevices = {&FL, &FR, &BL, &BR};
 
@@ -40,6 +60,18 @@ bool bin_intake = false;
 
 JsonDocument docTest;
 
+void checkPositionL() {
+  Encoder_Left.tick(); // just call tick() to check the state.
+}
+
+void checkPositionR() {
+  Encoder_Right.tick(); // just call tick() to check the state.
+}
+
+void checkPositionC() {
+  Encoder_Center.tick(); // just call tick() to check the state.
+}
+
 void setup() {
   Serial3.begin(115200);
 
@@ -48,14 +80,28 @@ void setup() {
   BR.Motor_enablePIDMode(false);
   BL.Motor_enablePIDMode(false);
 
-  pinMode(ledPin, arduino::OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(EL_PIN_1), checkPositionL,
+                  arduino::CHANGE);
+  attachInterrupt(digitalPinToInterrupt(EL_PIN_2), checkPositionL,
+                  arduino::CHANGE);
+
+  attachInterrupt(digitalPinToInterrupt(ER_PIN_1), checkPositionR,
+                  arduino::CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ER_PIN_2), checkPositionR,
+                  arduino::CHANGE);
+
+  attachInterrupt(digitalPinToInterrupt(EC_PIN_1), checkPositionC,
+                  arduino::CHANGE);
+  attachInterrupt(digitalPinToInterrupt(EC_PIN_2), checkPositionC,
+                  arduino::CHANGE);
+
+  // pinMode(ledPin, arduino::OUTPUT);
 }
 
 void loop() {
 
-  if (Serial3.available())
-  {
-    digitalWrite(ledPin, arduino::HIGH);
+  if (Serial3.available()) {
+    // digitalWrite(ledPin, arduino::HIGH);
 
     deserializeJson(doc, Serial3);
 
@@ -63,43 +109,40 @@ void loop() {
 
     Serial3.flush();
 
-    switch (msg_type)
-    {
-      case 0:
-        doc.clear();
-        doc["start_led"] = 0;
-        doc["deadwheel_stats"]["x"] = 6;
-        doc["deadwheel_stats"]["y"] = 100;
-        doc["deadwheel_stats"]["heading"] = 1.0;
-        serializeJson(doc, Serial3);
-        break;
-      case 1:
-        run = doc["run"];
+    switch (msg_type) {
+    case 0:
+      doc.clear();
+      doc["start_led"] = 0;
+      doc["deadwheel_stats"]["x"] = Encoder_Left.getPosition();
+      doc["deadwheel_stats"]["y"] = Encoder_Right.getPosition();
+      doc["deadwheel_stats"]["heading"] = Encoder_Center.getPosition();
+      serializeJson(doc, Serial3);
+      break;
+    case 1:
+      run = doc["run"];
 
-        if (run) {
-          float fl_speed = doc["motor_speeds"][0];
-          float fr_speed = doc["motor_speeds"][1];
-          float bl_speed = doc["motor_speeds"][2];
-          float br_speed = doc["motor_speeds"][3];
-          FL.Motor_setGoalSpeed(fl_speed);
-          FR.Motor_setGoalSpeed(fr_speed);
-          BL.Motor_setGoalSpeed(bl_speed);
-          BR.Motor_setGoalSpeed(br_speed);
-      
-          bin_intake = doc["bin_intake"];
-        } else {
-          FL.Motor_setGoalSpeed(0);
-          FR.Motor_setGoalSpeed(0);
-          BL.Motor_setGoalSpeed(0);
-          BR.Motor_setGoalSpeed(0);
-          bin_intake = false;
-        }
-        break;
+      if (run) {
+        float fl_speed = doc["motor_speeds"][0];
+        float fr_speed = doc["motor_speeds"][1];
+        float bl_speed = doc["motor_speeds"][2];
+        float br_speed = doc["motor_speeds"][3];
+        FL.Motor_setGoalSpeed(fl_speed);
+        FR.Motor_setGoalSpeed(fr_speed);
+        BL.Motor_setGoalSpeed(bl_speed);
+        BR.Motor_setGoalSpeed(br_speed);
+
+        bin_intake = doc["bin_intake"];
+      } else {
+        FL.Motor_setGoalSpeed(0);
+        FR.Motor_setGoalSpeed(0);
+        BL.Motor_setGoalSpeed(0);
+        BR.Motor_setGoalSpeed(0);
+        bin_intake = false;
+      }
+      break;
     }
 
     doc.clear();
-
-    digitalWrite(ledPin, arduino::LOW);
   }
 
   // UPDATE MOTOR POSITIONS WITH ENCODERS HERE:
