2be6f54eed3b591128c6e39c72f25932665882ec
Devyn04 <125822672+Devyn04@users.noreply.github.com>
2025-03-20 15:24:14 -0400

    Add files via upload

---
+++ include/Macros.h
@@ -0,0 +1,54 @@
+#ifndef MACROS_H_
+#define MACROS_H_
+
+// PCB Micros
+
+// Motor Drivers
+#define MD1_M1_PIN_1 4
+#define MD1_M1_PIN_2 5
+
+#define MD1_M2_PIN_1 2
+#define MD1_M2_PIN_2 3
+
+#define MD2_M1_PIN_1 19
+#define MD2_M1_PIN_2 18
+
+#define MD2_M2_PIN_1 23
+#define MD2_M2_PIN_2 22
+
+#define MD3_M1_PIN_1 28
+#define MD3_M1_PIN_2 29
+
+#define MD3_M2_PIN_1 24
+#define MD3_M2_PIN_2 25
+
+#define MD4_M1_PIN_1 37
+#define MD4_M1_PIN_2 36
+
+#define MD4_M2_PIN_1 15
+#define MD4_M2_PIN_2 14
+
+// UART/I2C
+#define UART_RX 34
+#define UART_TX 35
+
+#define UART_TX_I2C_SDA 17
+#define UART_RX_I2C_SCL 16
+
+// Encoders
+#define Encoder_1_PIN_1 0
+#define Encoder_1_PIN_2 1
+
+#define Encoder_2_PIN_1 6
+#define Encoder_2_PIN_2 7
+
+#define Encoder_3_PIN_1 8
+#define Encoder_3_PIN_2 9
+
+// Servos
+#define Servo_1 10
+#define Servo_2 11
+#define Servo_3 12
+#define Servo_4 33
+
+#endif
